3890220b0af9d8101b45369ef0119838660a6ced
theo <lesgooo1993@proton.me>
2023-12-06 00:16:32 +0530

    added sidebar button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script>
     import Icon from '$lib/icons/Icon.svelte'
+    import LeftArrow from '$lib/icons/LeftArrow.svelte'
     import Countdown from '$lib/components/Countdown.svelte';
+    
+    import { sidebarShown } from '$lib/store'
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
 <video src="loop.webm" autoplay loop class="fixed object-cover top-0 left-0 h-full w-full"/>
 
 <main class="z-10 fixed h-full w-full flex flex-col items-end justify-between p-8">
-    <Icon />
+    <div class="flex flex-col items-end gap-8">
+        <Icon />
+        <button on:click={() => $sidebarShown = !$sidebarShown}>
+            <LeftArrow />
+        </button>
+    </div>
 
     <img src="christ.png" alt="lmao" class="h-[140%] fixed left-8"/>
 
