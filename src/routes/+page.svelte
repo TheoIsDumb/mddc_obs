1575308c91e4a91bffc542f990a2232ceb3d237c
theo <lesgooo1993@proton.me>
2023-12-05 15:28:03 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Icon from '$lib/Icon.svelte'
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video src="vid.mp4" autoplay loop class="fixed top-0 left-0 w-full"/>
+
+<main class="z-10 fixed h-full w-full flex justify-end p-8">
+    <Icon />
+</main>
