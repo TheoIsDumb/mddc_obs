cb7c1b2988a0fa57e8ebe62783357864b132457e
theo <lesgooo1993@proton.me>
2023-12-05 15:35:20 +0530

    <video> background fix: object-fit: cover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
-<video src="vid.mp4" autoplay loop class="fixed top-0 left-0 w-full"/>
+<video src="vid.mp4" autoplay loop class="fixed object-cover top-0 left-0 h-full w-full"/>
 
 <main class="z-10 fixed h-full w-full flex justify-end p-8">
     <Icon />
