41ac345cf0caaf313970ac6cb859ddc4a82933e2
theo <lesgooo1993@proton.me>
2023-12-06 01:43:05 +0530

    added custom stream time thingy

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -9,22 +9,29 @@
     let timesList = ["06:00:00 AM", "05:30:00 PM", "06:45:00 PM"]
 </script>
 
-<div transition:fade class="sidebar h-full w-full fixed top-0 left-0 bg-black/40 z-20">
+<div transition:fade class="sidebar h-full w-full fixed top-0 left-0 bg-black/40 text-white z-20">
     <div class="cont absolute left-0 top-0 p-8 h-full bg-zinc-950/80">
-    <button on:click={() => $sidebarShown = !$sidebarShown}>
-        <Close />
-    </button>
+        <button on:click={() => $sidebarShown = !$sidebarShown}>
+            <Close />
+        </button>
 
-    <div class="choose text-white">
-        <h1 class="font-bold">Choose Stream Time</h1>
+        <div class="choose mb-4">
+            <h1 class="font-bold text-xl mb-2">Choose Stream Time</h1>
 
-        <div class="buttons flex flex-col items-center gap-2">
-            {#each timesList as item}
-                <button class="choose w-full border-white/50 border-2"
-                on:click={() => $streamTime = new Date().toLocaleDateString('en-US', options) + `, ${item}`}
-                >{item}</button>
-            {/each}
+            <div class="buttons flex flex-col items-center rounded border-white/50 border-2">
+                {#each timesList as item}
+                    <button class="choose w-full hover:bg-white hover:text-black border-b border-white/50 last:border-bottom-0
+                    rounded transition duration-300"
+                    on:click={() => $streamTime = new Date().toLocaleDateString('en-US', options) + `, ${item}`}
+                    >{item}</button>
+                {/each}
+            </div>
         </div>
-    </div>
+
+        <div class="custom">
+            <h1 class="font-bold text-xl mb-2">Choose Custom Stream Time</h1>
+            <input type="datetime-local" bind:value={$streamTime}
+            class="bg-transparent text-white border-white/50 border-2 w-full p-2"/>
+        </div>
     </div>
 </div>
