868ce3d0468459c7a10899e7c61aa13884e1d414
theo <lesgooo1993@proton.me>
2023-12-05 22:49:03 +0530

    sveltekit static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
     import Icon from '$lib/Icon.svelte'
+    
+    let timeString = "17:30:00"
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
-<video src="vid.mp4" autoplay loop class="fixed object-cover top-0 left-0 h-full w-full"/>
+<video src="loop.webm" autoplay loop class="fixed object-cover top-0 left-0 h-full w-full"/>
 
-<main class="z-10 fixed h-full w-full flex justify-end p-8">
+<main class="z-10 fixed h-full w-full flex flex-col items-end justify-between p-8">
     <Icon />
+    <h1 class="font-bold text-white">hello</h1>
 </main>
