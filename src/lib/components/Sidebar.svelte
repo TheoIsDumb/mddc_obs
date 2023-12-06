dbbd93b71f307257c714c05183f6cca7e1a0afeb
theo <lesgooo1993@proton.me>
2023-12-06 14:32:59 +0530

    added /malayalam; set up malayalam text select stuff in sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { sidebarShown, streamTime } from '$lib/store'
+    import { sidebarShown, streamTime, malayalamText } from '$lib/store'
     import Close from '$lib/icons/Close.svelte'
     import { fade, slide } from 'svelte/transition'
 
@@ -7,10 +7,15 @@
 
     let options = { timeZone: "Asia/Kolkata", year: 'numeric', month: '2-digit', day: '2-digit' }
     let timesList = ["06:00:00 AM", "05:30:00 PM", "06:45:00 PM"]
+
+    let malayalamTextOptions = [
+        { id: "ദിവ്യബലി" , text: "ദിവ്യബലി <br /> ഉടൻ ആരംഭിക്കുന്നതാണ്." },
+        { id: "പാദപൂജ" , text: "പാദപൂജ <br /> ഉടൻ ആരംഭിക്കുന്നതാണ്." },
+    ]
 </script>
 
 <div transition:fade class="sidebar h-full w-full fixed top-0 left-0 bg-zinc-950/60 text-white z-30">
-    <div in:slide={{ axis: 'x' }} out:slide={{ axis: 'x', duration: 100 }} class="cont absolute left-0 top-0 p-8 h-full bg-zinc-950/90 flex flex-col">
+    <div in:slide={{ axis: 'x' }} out:fade class="cont absolute left-0 top-0 p-8 h-full bg-zinc-950/90 flex flex-col">
         <button on:click={() => $sidebarShown = !$sidebarShown} class="mb-8">
             <Close />
         </button>
@@ -28,10 +33,24 @@
             </div>
         </div>
 
-        <div class="custom">
+        <div class="customTiming mb-4">
             <h1 class="font-bold text-xl mb-2">Choose Custom Stream Time</h1>
             <input type="datetime-local" bind:value={$streamTime}
             class="bg-transparent text-white border-white/50 border-2 w-full p-2"/>
+        </div>
+
+        <div class="malayalamText">
+            <h1 class="font-bold text-xl mb-2">Custom Malayalam Text</h1>
+
+            <div class="buttons flex flex-col items-center rounded border-white/50 border-2">
+                {#each malayalamTextOptions as opt}
+                    <button class="choose w-full hover:bg-white hover:text-black
+                    border-b border-white/50 last:border-bottom-0
+                    rounded transition duration-300"
+                    on:click={() => $malayalamText = opt.text} 
+                    >{opt.id}</button>
+                {/each}
+            </div>
         </div>
 
         <span class="mt-auto">
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,7 @@
 import { writable } from "svelte/store"
 
-export let sidebarShown = writable(false)
-
 let options = { timeZone: "Asia/Kolkata", year: 'numeric', month: '2-digit', day: '2-digit' }
+
+export let sidebarShown = writable(false)
 export let streamTime = writable(new Date().toLocaleDateString('en-US', options) + ', 05:30:00 PM')
+export let malayalamText = writable("ദിവ്യബലി <br /> ഉടൻ ആരംഭിക്കുന്നതാണ്.")
