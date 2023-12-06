57be1ab115fa68e57d1e5376093cca34fc20983f
theo <lesgooo1993@proton.me>
2023-12-06 12:34:08 +0530

    added fade across pages; added /stream button in home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,19 @@
   import "../app.pcss"
   import Sidebar from "$lib/components/Sidebar.svelte"
   import { sidebarShown } from '$lib/store'
+  import { fade } from "svelte/transition"
+
+  export let data
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
 <video src="loop.webm" autoplay loop class="fixed object-cover top-0 left-0 h-full w-full"/>
 
-<slot />
+{#key data}
+  <div in:fade={{ delay: 300 }} out:fade>
+    <slot />
+  </div>
+{/key}
 
 {#if $sidebarShown}
   <Sidebar />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<main class="z-10 fixed h-full w-full flex flex-col items-end justify-between p-8">
+    <a href="/stream" class="text-white font-bold text-2xl">stream</a>
+</main>
