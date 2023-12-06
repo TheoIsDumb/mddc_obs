8946dc7920a4322bcc3bb1ea62377e91d3f8aa6a
theo <lesgooo1993@proton.me>
2023-12-06 12:44:43 +0530

    page links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<main class="z-10 fixed h-full w-full flex flex-col items-end justify-between p-8">
-    <a href="/stream" class="text-white font-bold text-2xl">stream</a>
+<script>
+    let list = ["Stream", "Malayalam"]
+</script>
+
+<main class="z-10 fixed h-full w-full flex flex-col items-center justify-center p-8">
+    <div class="buttons flex flex-col border-white/50 border-2 rounded">
+        {#each list as i}
+            <a href={'/' + i.toLowerCase()}
+            class="text-white text-2xl border-b-2 last:border-b-0 border-white/50
+            hover:bg-white/40 hover:text-white transition duration-300
+            w-full px-4 py-2 text-center">
+            {i}
+        </a>
+        {/each}
+    </div>
 </main>
