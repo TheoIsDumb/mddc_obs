550a6de31e8dcc8a81dffdcaf6748d1ab77ed4fe
theo <lesgooo1993@proton.me>
2023-12-06 14:39:06 +0530

    /stream => /countdown; /malayalam => text

---
+++ src/routes/countdown/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Countdown from '$lib/components/Countdown.svelte';
+</script>
+
+<div class="flex h-full w-full flex-col items-end justify-between">
+    <img src="christ.png" alt="lmao" class="h-[140%] fixed left-8"/>
+
+    <div class="flex flex-col gap-2 mt-auto">
+        <Countdown />
+        <div class="bg-white h-[2px] mt-[-0.5rem] w-full"/>
+        <h6 class="font-bold text-right text-2xl text-white">Vettucaud Media Commission</h6>
+    </div>
+</div>
